25deffb2afe47aa9fcee93fc90d541b9f7e26aa5
liyah2 <aaliyahcloss@gmail.com>
2025-10-21 20:05:23 -0400

    finishing touches

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// =========Copyright section=============
+
 const body = document.body;
 
 let footer = document.createElement("footer");
@@ -12,27 +14,25 @@ copyright.innerHTML = `\u00A9 Aaliyah Closs ${thisYear}`;
 
 footer.appendChild(copyright);
 
-
-const skills = ["JavaScript", "HTML", "CSS", "Git" ,"Github"];
+// ===========Skills section==============
+const skills = ["JavaScript", "HTML", "CSS", "Git", "Github"];
 const skillSection = document.getElementById("skills");
 const skillsList = skillSection.querySelector(".skill-list");
 
 for (let i = 0; i < skills.length; i++) {
-  const skill = document.createElement("li");
-  skill.innerText = skills[i];
-  skillsList.appendChild(skill);
-  
+    const skill = document.createElement("li");
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
 }
 
-
-
-function activateMessageSection () {
+// ==============Message section====================
+function activateMessageSection() {
     const messageSection = document.getElementById("messages");
-    const messageList = messageSection.querySelector ("ul");
-    if(messageList.children.length === 0) {
-        messageSection.style.display = 'none';
+    const messageList = messageSection.querySelector("ul");
+    if (messageList.children.length === 0) {
+        messageSection.style.display = "none";
     } else {
-        messageSection.style.display = 'block';
+        messageSection.style.display = "block";
     }
 }
 
@@ -40,38 +40,30 @@ activateMessageSection();
 
 const messageForm = document.querySelector('form[name="leave_message"]');
 
-messageForm.addEventListener("submit" , function(event) {
+messageForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const usersName = event.target.usersName.value;
     const usersEmail = event.target.usersEmail.value;
     const usersMessage = event.target.usersMessage.value;
 
-
-
-    console.log("Name: ", usersName );
-    console.log("Email: ", usersEmail );
-    console.log("Message: ", usersMessage );
-
-
+    console.log("Name: ", usersName);
+    console.log("Email: ", usersEmail);
+    console.log("Message: ", usersMessage);
 
     const messageSection = document.getElementById("messages");
     const messageList = messageSection.querySelector("ul");
 
-
-
     const newMessage = document.createElement("li");
-    newMessage.innerHTML= `<a href="mailto:${usersEmail}">${usersName}</a>: <span> ${usersMessage}</span>`;
-
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>: <span> ${usersMessage}</span>`;
 
     const editButton = document.createElement("button");
     editButton.innerText = "edit";
-    editButton.className = "edit-btn"
-    editButton.type = "button"
+    editButton.className = "edit-btn";
+    editButton.type = "button";
 
-
-    editButton.addEventListener("click", function() {
+    editButton.addEventListener("click", function () {
         const messageSpan = this.parentNode.querySelector("span");
-        const newText = prompt("Edit your message: " , messageSpan.innerText);
+        const newText = prompt("Edit your message: ", messageSpan.innerText);
         if (newText !== null) {
             messageSpan.innerText = newText;
         }
@@ -79,22 +71,16 @@ messageForm.addEventListener("submit" , function(event) {
 
     newMessage.appendChild(editButton);
 
-
-
     const removeButton = document.createElement("button");
     removeButton.innerText = "remove";
     removeButton.className = "remove-btn";
     removeButton.type = "button";
 
-
-
-    removeButton.addEventListener("click", function() {
+    removeButton.addEventListener("click", function () {
         const entry = this.parentNode;
         entry.remove();
         activateMessageSection();
     });
-
-
 
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
@@ -104,28 +90,38 @@ messageForm.addEventListener("submit" , function(event) {
     messageForm.reset();
 });
 
-// ====Creating Fetch====
+// ====Creating Fetch/Repositories====
 
 fetch("https://api.github.com/users/liyah2/repos")
-.then ((response) => {
-    return response.json();
-})
+    .then((response) => {
+        return response.json();
+    })
 
-.then((repositories)=> {
-    console.log("Repositories: ", repositories);
+    .then((repositories) => {
+        console.log("Repositories: ", repositories);
 
-    const projectSection = document.getElementById("projects");
-    const projectList = projectSection.querySelector("ul");
-    projectList.innerHTML = "";
+        const projectSection = document.getElementById("projects");
+        const projectList = projectSection.querySelector("ul");
+        projectList.innerHTML = "";
 
-    for (let i = 0; i < repositories.length; i++) {
-        const project = document.createElement("li");
+        for (let i = 0; i < repositories.length; i++) {
+            const project = document.createElement("li");
 
-        project.innerHTML = repositories[i].name;
-        projectList.appendChild(project);
-    }
-})
+            project.innerHTML = repositories[i].name;
+            projectList.appendChild(project);
+        }
+    })
 
-.catch ((error) => {
-    console.log("Error: ", error)
-})
+    .catch((error) => {
+        console.log("Error:Something went wrong. Please check back later. ", error);
+    });
+
+// ====Hamburger bar========================
+
+const hamMenu = document.querySelector(".ham-menu");
+const navbarMenu = document.querySelector(".navbar ul");
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active"); 
+    navbarMenu.classList.toggle("active"); 
+});
