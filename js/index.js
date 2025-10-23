408088e91dc158c22fc07938a0c1365017efd415
liyah2 <aaliyahcloss@gmail.com>
2025-10-22 23:19:56 -0400

    additional changes

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
 // =========Copyright section=============
-
-const body = document.body;
-
-let footer = document.createElement("footer");
-body.appendChild(footer);
+// Adds copyright to the bottom of the page
 
 const today = new Date();
 const thisYear = today.getFullYear();
-
+const body = document.body;
+const footer = document.createElement("footer");
 const copyright = document.createElement("p");
 
 copyright.innerHTML = `\u00A9 Aaliyah Closs ${thisYear}`;
-
 footer.appendChild(copyright);
+body.appendChild(footer);
+
 
 // ===========Skills section==============
+// Populate skills section with items assigned to skills
 const skills = ["JavaScript", "HTML", "CSS", "Git", "Github"];
 const skillSection = document.getElementById("skills");
 const skillsList = skillSection.querySelector(".skill-list");
@@ -26,22 +25,23 @@ for (let i = 0; i < skills.length; i++) {
 }
 
 // ==============Message section====================
+// Section to handle forms submission  
 function activateMessageSection() {
     const messageSection = document.getElementById("messages");
     const messageList = messageSection.querySelector("ul");
+
     if (messageList.children.length === 0) {
         messageSection.style.display = "none";
     } else {
         messageSection.style.display = "block";
     }
 }
-
 activateMessageSection();
 
 const messageForm = document.querySelector('form[name="leave_message"]');
-
 messageForm.addEventListener("submit", function (event) {
     event.preventDefault();
+
     const usersName = event.target.usersName.value;
     const usersEmail = event.target.usersEmail.value;
     const usersMessage = event.target.usersMessage.value;
@@ -54,8 +54,10 @@ messageForm.addEventListener("submit", function (event) {
     const messageList = messageSection.querySelector("ul");
 
     const newMessage = document.createElement("li");
-    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>: <span> ${usersMessage}</span>`;
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>: 
+    <span> ${usersMessage}</span>`;
 
+// Handles edit and remove button
     const editButton = document.createElement("button");
     editButton.innerText = "edit";
     editButton.className = "edit-btn";
@@ -69,8 +71,6 @@ messageForm.addEventListener("submit", function (event) {
         }
     });
 
-    newMessage.appendChild(editButton);
-
     const removeButton = document.createElement("button");
     removeButton.innerText = "remove";
     removeButton.className = "remove-btn";
@@ -81,17 +81,15 @@ messageForm.addEventListener("submit", function (event) {
         entry.remove();
         activateMessageSection();
     });
-
-    newMessage.appendChild(removeButton);
+    newMessage.appendChild(editButton, removeButton);
     messageList.appendChild(newMessage);
 
     activateMessageSection();
-
     messageForm.reset();
 });
 
-// ====Creating Fetch/Repositories====
-
+// ========GitHub Project Section==============
+// Retrives projects from GitHub
 fetch("https://api.github.com/users/liyah2/repos")
     .then((response) => {
         return response.json();
@@ -99,25 +97,22 @@ fetch("https://api.github.com/users/liyah2/repos")
 
     .then((repositories) => {
         console.log("Repositories: ", repositories);
-
         const projectSection = document.getElementById("projects");
         const projectList = projectSection.querySelector("ul");
         projectList.innerHTML = "";
 
         for (let i = 0; i < repositories.length; i++) {
             const project = document.createElement("li");
-
             project.innerHTML = repositories[i].name;
             projectList.appendChild(project);
         }
     })
-
     .catch((error) => {
         console.log("Error:Something went wrong. Please check back later. ", error);
     });
 
 // ====Hamburger bar========================
-
+// allows hamburger button to appear for smaller screens
 const hamMenu = document.querySelector(".ham-menu");
 const navbarMenu = document.querySelector(".navbar ul");
 
